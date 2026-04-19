8de3be649be28cc6b7499bcea6a55bfe29454e71
zhangbin0301 <zhangbin0301@qq.com>
2026-04-19 13:13:28 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -817,7 +817,7 @@ class TeoBot {
       "📋 Teoheberg 状况报告 ",
       `🪙 领币任务: ${this.stats.claimProgress} (${earnStatus})`,
       `🔄 续期执行: ${renewStatus}`,
-      "─────────────────────────",
+      "────────────────────",
       `💵 最初余额: ${initialCoins} Credits`,
       `🏦 当前余额: ${finalCoins} Credits`,
       `⏳ 剩余时间: ${remainingTime}`,
